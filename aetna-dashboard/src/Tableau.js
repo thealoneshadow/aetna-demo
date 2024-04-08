c1eac84e15faae5805309ddc23560573aa44295e
divyanshuelred <divyanshu@elred.io>
2024-04-08 15:56:30 +0530

    test

--- aetna-dashboard/src/Tableau.js
+++ aetna-dashboard/src/Tableau.js
@@ -1,37 +1,9 @@
-<div className="accordion">
-  <div className="accordion-header">
-    <div className="header-left">
-      {/* Heading with icon */}
-      <h2>Heading</h2>
-      <img src="icon.png" alt="icon" />
-    </div>
-    <div className="header-right">
-      {/* Three lines with space between */}
-      <div className="line"></div>
-      <div className="line"></div>
-      <div className="line"></div>
-    </div>
-  </div>
-  <div className="accordion-content">
-    {/* KPIs section */}
-    <div className="kpi-container">
-      <div className="kpi-left">
-        {/* Heading with paragraph text */}
-        <h3>KPIs Left Section</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae
-          velit eget lacus dictum dapibus.
-        </p>
-      </div>
-      <div className="kpi-divider"></div> {/* Line between KPI sections */}
-      <div className="kpi-right">
-        {/* Heading with paragraph text */}
-        <h3>KPIs Right Section</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae
-          velit eget lacus dictum dapibus.
-        </p>
-      </div>
-    </div>
+<div class="container">
+  <h2 class="heading">Heading</h2>
+  <div class="content">
+    <p>This is line 1 of content.</p>
+    <p>This is line 2 of content.</p>
+    <p>This is line 3 of content.</p>
+    <p>This is line 4 of content.</p>
   </div>
 </div>;
