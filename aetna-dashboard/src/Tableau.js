0f8ccf514c7a70ea5a6a108934201032695202e3
divyanshuelred <divyanshu@elred.io>
2024-04-09 14:43:00 +0530

    test

--- aetna-dashboard/src/Tableau.js
+++ aetna-dashboard/src/Tableau.js
@@ -1,53 +1,43 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
-const Navbar = () => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
+// Define your styled component
+const StyledButton = styled.button`
+  /* Your default styles here */
+  background-color: ${(props) => (props.selected ? "blue" : "green")};
+  color: white;
+  padding: 10px;
+  margin: 5px;
+  border: none;
+  cursor: pointer;
 
-  const toggleDropdown = () => {
-    setDropdownOpen(!dropdownOpen);
-  };
+  &:hover {
+    background-color: ${(props) => (props.selected ? "darkblue" : "darkgreen")};
+  }
+`;
 
-  const NavbarItem = ({ title, children }) => {
-    const [subDropdownOpen, setSubDropdownOpen] = useState(false);
+const ButtonList = () => {
+  const [selectedButtonIndex, setSelectedButtonIndex] = useState(null);
 
-    const toggleSubDropdown = () => {
-      setSubDropdownOpen(!subDropdownOpen);
-    };
-
-    return (
-      <li className="navbar-item">
-        <div onClick={toggleSubDropdown} className="navbar-item-title">
-          {title}
-        </div>
-        {subDropdownOpen && (
-          <ul className="sub-dropdown">
-            {children.map((item, index) => (
-              <li key={index}>{item}</li>
-            ))}
-          </ul>
-        )}
-      </li>
-    );
+  const handleButtonClick = (index) => {
+    setSelectedButtonIndex(index);
   };
 
   return (
-    <nav className="navbar">
-      <ul className="navbar-list">
-        <NavbarItem title="Item 1">
-          {["Sub Item 1", "Sub Item 2", "Sub Item 3"]}
-        </NavbarItem>
-        <NavbarItem title="Item 2">
-          {["Sub Item 1", "Sub Item 2", "Sub Item 3"]}
-        </NavbarItem>
-        <NavbarItem title="Item 3">
-          {["Sub Item 1", "Sub Item 2", "Sub Item 3"]}
-        </NavbarItem>
-        <NavbarItem title="Item 4">
-          {["Sub Item 1", "Sub Item 2", "Sub Item 3"]}
-        </NavbarItem>
-      </ul>
-    </nav>
+    <div>
+      {[0, 1, 2, 3].map((index) => (
+        <StyledButton
+          key={index}
+          className={
+            selectedButtonIndex === index ? "rs-btn selected" : "rs-btn"
+          }
+          onClick={() => handleButtonClick(index)}
+        >
+          Button {index + 1}
+        </StyledButton>
+      ))}
+    </div>
   );
 };
 
-export default Navbar;
+export default ButtonList;
