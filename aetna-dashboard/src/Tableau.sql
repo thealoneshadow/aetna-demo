263f719be58bf314b36906dedd91535f38f804e8
divyanshuelred <divyanshu@elred.io>
2024-05-28 14:00:31 +0530

    test

--- aetna-dashboard/src/Tableau.sql
+++ aetna-dashboard/src/Tableau.sql
@@ -423,3 +423,13 @@ export default CardList;
 }
 
 
+table tr:last-child td:first-child {
+    border: 2px solid orange;
+    border-bottom-left-radius: 10px;
+}
+    
+table tr:last-child td:last-child {
+    border: 2px solid green;
+    border-bottom-right-radius: 10px;
+}
+
