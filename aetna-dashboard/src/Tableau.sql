77a90c6b216d27b1d6a646477b46932e0ce60fbd
divyanshuelred <divyanshu@elred.io>
2024-07-22 17:51:08 +0530

    dfghdfg

--- aetna-dashboard/src/Tableau.sql
+++ aetna-dashboard/src/Tableau.sql
@@ -1,1 +1,11 @@
 defaultValue={[dayjs('2015/01/01', dateFormat), dayjs('2015/01/01', dateFormat)]}
+
+const currentDate = new Date();
+
+// Get the date from exactly one week ago
+const lastWeekDate = new Date();
+lastWeekDate.setDate(currentDate.getDate() - 7);
+
+// Format the dates as YYYY-MM-DD
+const currentFormattedDate = currentDate.toISOString().slice(0, 10);
+const lastWeekFormattedDate = lastWeekDate.toISOString().slice(0, 10);
