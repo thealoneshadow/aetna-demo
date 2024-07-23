3e3341adb63d1e335c33ed8d6454a697347c0a29
divyanshuelred <divyanshu@elred.io>
2024-07-23 13:03:44 +0530

    sd

--- aetna-dashboard/src/Tableau.sql
+++ aetna-dashboard/src/Tableau.sql
@@ -9,3 +9,43 @@ lastWeekDate.setDate(currentDate.getDate() - 7);
 // Format the dates as YYYY-MM-DD
 const currentFormattedDate = currentDate.toISOString().slice(0, 10);
 const lastWeekFormattedDate = lastWeekDate.toISOString().slice(0, 10);
+
+
+import React from 'react';
+import { Button, message, Space } from 'antd';
+
+const App: React.FC = () => {
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const success = () => {
+    messageApi.open({
+      type: 'success',
+      content: 'This is a success message',
+    });
+  };
+
+  const error = () => {
+    messageApi.open({
+      type: 'error',
+      content: 'This is an error message',
+    });
+  };
+
+  const warning = () => {
+    messageApi.open({
+      type: 'warning',
+      content: 'This is a warning message',
+    });
+  };
+
+  return (
+    <>
+      {contextHolder}
+      <Space>
+        <Button onClick={success}>Success</Button>
+        <Button onClick={error}>Error</Button>
+        <Button onClick={warning}>Warning</Button>
+      </Space>
+    </>
+  );
+};
