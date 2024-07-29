35d85f36f35d714625c1790f92f339168f99cc57
divyanshuelred <divyanshu@elred.io>
2024-07-29 11:39:32 +0530

    dfg

--- aetna-dashboard/src/Tableau.sql
+++ aetna-dashboard/src/Tableau.sql
@@ -7,3 +7,12 @@ FROM (VALUES
 ) AS newUsers(userName, email)
 LEFT JOIN userList ON newUsers.email = userList.email
 WHERE userList.email IS NULL;
+
+
+INSERT INTO userList (userId, userName, email)
+SELECT 'new_user_id', 'New User', 'new.user@example.com'
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM userList
+    WHERE email = 'new.user@example.com'
+);
