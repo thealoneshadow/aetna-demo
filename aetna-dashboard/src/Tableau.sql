8a654b430c10b3f3a9a3ff59df5531ebd10f2a28
divyanshuelred <divyanshu@elred.io>
2024-07-30 15:20:51 +0530

    dfghgh

--- aetna-dashboard/src/Tableau.sql
+++ aetna-dashboard/src/Tableau.sql
@@ -30,3 +30,21 @@ import BarChart from "../components/BarChart";
 import "./styles.css";
 
 Chart.register(CategoryScale);
+
+
+ labels: Data.map((data) => data.year), 
+    datasets: [
+      {
+        label: "Users Gained ",
+        data: Data.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          &quot;#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0"
+        ],
+        borderColor: "black",
+        borderWidth: 2
+      }
+    ]
