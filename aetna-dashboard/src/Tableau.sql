0d9dcb3a3f4b66a4ccbd9d18867968eb0d2bfa13
divyanshuelred <divyanshu@elred.io>
2024-08-22 13:14:45 +0530

    zxc

--- aetna-dashboard/src/Tableau.sql
+++ aetna-dashboard/src/Tableau.sql
@@ -64,11 +64,17 @@ const CheckCookieComponent = () => {
     }
   }, []);
 
-  function getCookieValue(name) 
-    {
-      const regex = new RegExp(`(^| )${name}=([^;]+)`)
-      const match = document.cookie.match(regex)
-      if (match) {
-        return match[2]
+const checkCookie = () => {
+      try {
+        const cookies = newWindow.document.cookie;
+        if (cookies.includes('desired_cookie_name')) {
+          clearInterval(cookieInterval);
+          newWindow.close();
+          window.location.reload(); // Reload the main window
+        }
+      } catch (error) {
+        console.error('Error accessing the new window\'s cookies:', error);
       }
-   }
+    };
+
+    const cookieInterval = setInterval(checkCookie, 1000);
