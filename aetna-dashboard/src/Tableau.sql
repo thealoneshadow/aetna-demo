3810516c6a53ddc97e4140da644907d540f204f3
divyanshuelred <divyanshu@elred.io>
2024-08-22 12:46:27 +0530

    rer

--- aetna-dashboard/src/Tableau.sql
+++ aetna-dashboard/src/Tableau.sql
@@ -63,3 +63,12 @@ const CheckCookieComponent = () => {
       console.log(`${cookieName} does not exist`);
     }
   }, []);
+
+  function getCookieValue(name) 
+    {
+      const regex = new RegExp(`(^| )${name}=([^;]+)`)
+      const match = document.cookie.match(regex)
+      if (match) {
+        return match[2]
+      }
+   }
