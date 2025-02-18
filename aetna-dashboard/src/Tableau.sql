c75ad01a23850e59e3f147a5fd8ff2a65c6f9a21
TheAloneShadow <amayauta12@gmail.com>
2025-02-18 16:44:17 +0530

    Update Tableau.sql

--- aetna-dashboard/src/Tableau.sql
+++ aetna-dashboard/src/Tableau.sql
@@ -25,3 +25,24 @@ filters
   })
   .filter(Boolean) // Remove any empty strings from invalid filters
   .join(" ");
+
+
+
+
+filters
+  .filter(filter => filter.column && filter.value) // Exclude incomplete filters
+  .map((filter, index) => {
+    const logicalOp = index > 0 ? ` ${filter.logicalOp} ` : "";
+    const columnType = columns.find(col => col.name === filter.column)?.type;
+    
+    let formattedValue = filter.value;
+    if (columnType === "date") {
+      formattedValue = formatDate(filter.value); // Ensure date is formatted properly
+      formattedValue = `'${formattedValue}'`; // Wrap date values in single quotes
+    } else if (columnType === "string") {
+      formattedValue = `'${formattedValue}'`; // Wrap strings in single quotes
+    }
+
+    return `${logicalOp}${filter.column} ${filter.operator} ${formattedValue}`;
+  })
+  .join(" ");
