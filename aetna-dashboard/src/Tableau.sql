4575360e78d885b4d22227baa1be93d79296d7dc
TheAloneShadow <amayauta12@gmail.com>
2025-04-16 16:47:05 +0530

    Update Tableau.sql

--- aetna-dashboard/src/Tableau.sql
+++ aetna-dashboard/src/Tableau.sql
@@ -2,11 +2,12 @@ function areStringsSame(str1, str2) {
   const clean = str => {
     if (!str) return '';
     str = str.trim();
-    
-    // Remove starting and ending quotes if same
+
     const first = str[0];
     const last = str[str.length - 1];
-    if ((first === last) && (`'"``".includes(first))) {
+
+    // Remove wrapping quotes if they match and are one of `' " or `
+    if ((first === last) && (`'"```.includes(first))) {
       str = str.slice(1, -1);
     }
 
