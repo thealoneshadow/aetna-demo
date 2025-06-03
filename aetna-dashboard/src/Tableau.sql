df980f668fe5f5081950c557256110756fdad1f4
TheAloneShadow <amayauta12@gmail.com>
2025-06-03 15:39:25 +0530

    Update Tableau.sql

--- aetna-dashboard/src/Tableau.sql
+++ aetna-dashboard/src/Tableau.sql
@@ -1,1 +1,12 @@
-<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+function areArraysEqual(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  const stringify = obj => JSON.stringify(obj);
+  const set1 = new Set(arr1.map(stringify));
+  const set2 = new Set(arr2.map(stringify));
+
+  for (let item of set1) {
+    if (!set2.has(item)) return false;
+  }
+  return true;
+}
