96d161cea31f323b1fd83bab4aa911011292cdb3
TheAloneShadow <amayauta12@gmail.com>
2025-06-17 22:01:34 +0530

    Update Tableau.sql

--- aetna-dashboard/src/Tableau.sql
+++ aetna-dashboard/src/Tableau.sql
@@ -1,2 +1,12 @@
-const labels = input.map(item => item.name);
-  const dataValues = input.map(item => item.value);
+const totals = {};
+
+  arr.forEach(obj => {
+    Object.entries(obj).forEach(([key, value]) => {
+      if (value !== "") {
+        totals[key] = (totals[key] || 0) + Number(value);
+      }
+    });
+  });
+
+  const labels = Object.keys(totals);
+  const dataValues = Object.values(totals);
