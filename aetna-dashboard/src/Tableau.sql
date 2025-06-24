5dfbb776fea97b8781af2cf58d8bf2882dda806b
TheAloneShadow <amayauta12@gmail.com>
2025-06-24 14:58:56 +0530

    Update Tableau.sql

--- aetna-dashboard/src/Tableau.sql
+++ aetna-dashboard/src/Tableau.sql
@@ -1,49 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import { Collapse } from 'antd';
+<ReactMarkdown
+      children={markdownText}
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || '');
 
-const Demo = ({ data }) => {
-  const [activeKeys, setActiveKeys] = useState({}); // store activeKey for each index
-
-  useEffect(() => {
-    // On data change, only open the 2nd panel (key = '2') of the last Collapse
-    const newActiveKeys = {};
-    const lastIndex = data.length - 1;
-    newActiveKeys[lastIndex] = ['2'];
-    setActiveKeys(newActiveKeys);
-  }, [data]);
-
-  const handleChange = (index) => (keys) => {
-    setActiveKeys((prev) => ({
-      ...prev,
-      [index]: keys, // allow user to interact per collapse
-    }));
-  };
-
-  return (
-    <div>
-      {data.map((item, index) => {
-        const items = [
-          {
-            key: '1',
-            label: `Panel 1 of item ${index + 1}`,
-            children: `This is the first panel of item ${index + 1}`,
-          },
-          {
-            key: '2',
-            label: `Panel 2 of item ${index + 1}`,
-            children: `This is the second panel of item ${index + 1}`,
-          },
-        ];
-
-        return (
-          <Collapse
-            key={index}
-            items={items}
-            activeKey={activeKeys[index] || []}
-            onChange={handleChange(index)}
-          />
-        );
-      })}
-    </div>
-  );
-};
+          return !inline && match ? (
+            <SyntaxHighlighter
+              style={oneDark}
+              language={match[1]}
+              PreTag="div"
+              {...props}
+            >
+              {String(children).replace(/\n$/, '')}
+            </SyntaxHighlighter>
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    />
