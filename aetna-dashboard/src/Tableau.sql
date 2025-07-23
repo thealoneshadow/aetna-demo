071d894ac25d2e59599777311c0889fc6a020a93
TheAloneShadow <amayauta12@gmail.com>
2025-07-23 17:28:11 +0530

    Update Tableau.sql

--- aetna-dashboard/src/Tableau.sql
+++ aetna-dashboard/src/Tableau.sql
@@ -1,2 +1,5 @@
+I hope this email finds you well.
 
-const cleaned = input.replace(/\[Orignal User Question\][\s\S]*?\[Pseudo Query\]/g, "");
+We are currently facing persistent timeout issues with our "Magic Portal" application, specifically in the development environment.
+
+To help us investigate and resolve this, could you please provide the pod configuration details for the "Magic Portal" from both the QA and Production servers? We would like to compare the configurations to identify any potential discrepancies that might be contributing to the performance problems.
